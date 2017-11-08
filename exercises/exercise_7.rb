99a7b49229d40cb8d292e2cbf57c875fa3735a39
broodmeister <isaiahchrst80@gmail.com>
2017-11-08 05:44:39 +0000

    Completed steps 1 & 2 of exercise 8.

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -11,9 +11,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class ApparelValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.mens_apparel || record.womens_apparel
+      record.errors[:base] << "a store has to have either men's or women's apparel"
+    end
+  end
+end
+
 class Store < ActiveRecord::Base
   validates :name,            length: { minimum: 3 }
   validates :annual_revenue,  numericality: {greater_than_or_equal_to: 0}
+
+  include ActiveModel::Validations
+  validates_with ApparelValidator
 end
 
 class Employee < ActiveRecord::Base
@@ -22,6 +33,10 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, inclusion: 40..200
   validates :store_id,    presence: true
 end
+
+invalidStore = Store.new name:"this", annual_revenue: 10, mens_apparel:false, womens_apparel:false
+invalidStore.save
+pp invalidStore.errors
 
 print 'New store name:'
 newName = STDIN.gets.chomp
